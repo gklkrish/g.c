10fa3ea211a1da24ff5aeaf9172c8ead99a9fe92
gklkrish <35652631+gklkrish@users.noreply.github.com>
2018-02-09 16:44:47 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+void main()
+{
+int g[5],a={2,3,5,7,11};
+if(g==a)
+printf(" \n The Number is Prime Number");
+scanf("%d",&g);
+printf(" \n The Number Is Not Prime Number");
+scanf("%d",&a);
+return 0;
+}
